bbc07f31284b4aa86bf8412b2e6e0602a7d7528a
calyptr4 <alessandrabaires@gmail.com>
2026-04-02 19:47:34 -0700

    a57

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@ int main()
 {
 	int n1, n2;
 	int N, sum;
-
+	cout << "First two integers: " << endl;
 	cin >> n1 >> n2;
+	cout << "How many times will integers be added?" << endl;
 	cin >> N;
+	cout << n1 << " " << n2 << " ";
+	for (int i=0; i<N-2; i++){
+		sum = n1 + n2;
+		cout << sum << " ";
+		n1 = n2;
+		n2 = sum;
+	}
 
-	// TODO
-	// TODO
+	cout << endl;
+	return 0;
 }
